89bc45be1e33d2816093a60359e2987b8c4930dd
ruleless <ruleless@126.com>
2016-08-15 19:23:05 +0800

    KcpTunnel

--- src/KcpTunnel.h
+++ src/KcpTunnel.h
@@ -1,18 +1,40 @@
 #ifndef __KCPTUNNEL_H__
 #define __KCPTUNNEL_H__
 
+#include "FasttunBase.h"
+#include "ikcp.h"
+
 NAMESPACE_BEG(tun)
 
-class KcpTunnul : public Tunnel
+class KcpTunnul
 {
   public:
+	struct Handler 
+	{
+		virtual void onRecv(KcpTunnul *pTunnel, const void *data, size_t datelen) = 0;
+	};
+
     KcpTunnul()
-			:Tunnel()
+			:mKcpCb(NULL)
+			,mHandler(NULL)
 	{}
 	
     virtual ~KcpTunnul();
 
-	
+	bool create();
+	void shutdown();
+
+	int send(const void *data, size_t datalen);
+
+	inline void setEventHandler(Handler *h)
+	{
+		mHandler = h;
+	}
+
+	void update(uint32 current);
+  private:
+	ikcpcb *mKcpCb;
+	Handler *mHandler;
 };
 
 NAMESPACE_END // namespace tun
